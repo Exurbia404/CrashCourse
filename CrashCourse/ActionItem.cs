a02355b001242da38f2629d388dac88868b847c1
Tom <454147@student.fontys.nl>
2024-06-20 11:36:07 +0200

    Added CrashCourse project

---
+++ CrashCourse/ActionItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourse
+{
+    public class ActionItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Difficulty { get; set; }
+        public Priority Priority { get; set; }
+
+        public ActionItem(string name, string description, int difficulty, Priority priority)
+        {
+            Name = name;
+            Description = description;
+            Difficulty = difficulty;
+            Priority = priority;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Description: " + Description + ", Difficulty: " + Difficulty + ", Priority: " + Priority;
+        }
+    }
+}
---
+++ CrashCourse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ToDoListForm newForm = new ToDoListForm(); 
+            newForm.ShowDialog();
+        }
+    }
+}
